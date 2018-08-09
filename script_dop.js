8889a211bb0f2ddb0550d815f890582221b2bbe5
Maksim <anger65@gmail.com>
2018-08-10 00:20:23 +0300

    Дополнительное первое задание (исправлено)

--- script_dop.js
+++ script_dop.js
@@ -1,12 +1,10 @@
-{
 	let num = 33721;
 	let arr = num.toString().split("");
 	let comp = 1;
 
-	for (i = 0; i < arr.length; i++) {
+	for (let i = 0; i < arr.length; i++) {
 		comp = comp * arr[i];
 	}
 	
 	console.log(comp);
 	alert( Math.pow(comp, 3).toString().substring(0, 2));
-}
